19a7725d3e78419372f559d20e01315db8cd9fb6
Shakti184 <shaktimaddeshiya184@gmail.com>
2025-12-28 21:18:09 +0530

    Fix base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/shree-ram-events/", // <--- ADD THIS LINE (Must match your Repo Name)
+  base: "/", // <--- ADD THIS LINE (Must match your Repo Name)
 })
